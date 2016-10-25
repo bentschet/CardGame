d98bfabaedb559c950fe8fd1767777065669289d
Benjamin Tschetter <benjamintschetter3@gmail.com>
2016-10-25 13:14:50 -0600

    Created Card.swift and PlayingCard.swift
    What a smelly commit.

---
+++ CardGame/Model/Card.swift
@@ -0,0 +1,9 @@
+//
+//  Card.swift
+//  CardGame
+//
+//  Created by Tschetter, Benjamin on 10/25/16.
+//  Copyright © 2016 Tschetter, Benjamin. All rights reserved.
+//
+
+import Foundation
---
+++ CardGame/Model/PlayingCard.swift
@@ -0,0 +1,9 @@
+//
+//  PlayingCard.swift
+//  CardGame
+//
+//  Created by Tschetter, Benjamin on 10/25/16.
+//  Copyright © 2016 Tschetter, Benjamin. All rights reserved.
+//
+
+import Foundation
