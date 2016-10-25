41c786a3fe129aa20c7a664bcf937e274216f55c
Benjamin Tschetter <benjamintschetter3@gmail.com>
2016-10-25 13:43:02 -0600

    initialized PlayingCard.swift
    Noah didn’t say anything but Jackson sent me an instagram meme. Thanks,
    Jackson.

--- CardGame/Model/PlayingCard.swift
+++ CardGame/Model/PlayingCard.swift
@@ -6,4 +6,26 @@
 //  Copyright © 2016 Tschetter, Benjamin. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class PlayingCard : Card
+{
+    private var rank : Int
+    private var suit : String
+    private var color : UIColor
+    
+    override init()
+    {
+        rank = 0
+        suit = ""
+        color = UIColor()
+        super.init()
+    }
+    
+    override func toString() -> String
+    {
+        let description = "The playing card rank is: \(rank) and its suit is \(suit). It has a color of \(color)."
+        
+        return description
+    }
+}
