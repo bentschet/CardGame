15f8257a7c82344030be98f6d72226a28829e7cb
Benjamin Tschetter <benjamintschetter3@gmail.com>
2016-10-31 12:43:52 -0600

    added func shuffleDeck and func orderDeck

---
+++ CardGame/Deck.swift
@@ -0,0 +1,14 @@
+//
+//  Deck.swift
+//  CardGame
+//
+//  Created by Tschetter, Benjamin on 10/31/16.
+//  Copyright © 2016 Tschetter, Benjamin. All rights reserved.
+//
+
+import Foundation
+
+class Deck
+{
+    
+}
---
+++ CardGame/PlayingCardDeck.swift
@@ -0,0 +1,28 @@
+//
+//  PlayingCardDeck.swift
+//  CardGame
+//
+//  Created by Tschetter, Benjamin on 10/31/16.
+//  Copyright © 2016 Tschetter, Benjamin. All rights reserved.
+//
+
+import Foundation
+
+class PlayingCardDeck : Deck
+{
+    internal lazy var cards = [PlayingCard]()
+    
+    override init()
+    {
+        super.init()
+    }
+    
+    func shuffleDeck() -> Void
+    {
+        
+    }
+    func orderDeck() -> Void
+    {
+        
+    }
+}
