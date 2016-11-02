97975d4f38a540ce11f8b476240cfa5c555c7cda
Benjamin Tschetter <benjamintschetter3@gmail.com>
2016-11-02 13:53:45 -0600

    Added cardback to storyboard

---
+++ CardGameController.swift
@@ -0,0 +1,19 @@
+//
+//  CardGameController.swift
+//  CardGame
+//
+//  Created by Tschetter, Benjamin on 11/2/16.
+//  Copyright © 2016 Tschetter, Benjamin. All rights reserved.
+//
+
+import UIKit
+
+class CardGameController : UIViewController
+{
+    override func viewDidLoad() -> Void
+    {
+        let tempCard = Card()
+        print(tempCard.toString())
+    }
+}
+
