9679d3ec187fff146f61e67a2ab98adeb1522849
Benjamin Tschetter <benjamintschetter3@gmail.com>
2016-11-09 12:08:46 -0700

    added code to fund shuffleDeck
    also, this is my first post-election commit. The first commit in a
    world where Donald Trump is the president elect.

--- CardGame/Deck.swift
+++ CardGame/Deck.swift
@@ -14,7 +14,24 @@ class Deck
     
     func shuffleDeck() -> Void
     {
+        //Creates a temporary variable to hold Cards.
+        var tempDeck = [Card]()
         
+        //Repeat until cards data member is empty.
+        while self.cards.count > 0
+        {
+            //Get a random number between 0...cards.count-1
+            let randomIndex = Int (arc4random() % (UInt32)(cards.count))
+            
+            //Remove the card at that index from the back.
+            let removedCard = cards.removeAtIndex(randomIndex)
+            
+            //Place that card in the temporary array.
+            tempDeck.append(removedCard)
+        }
+        
+        //Replace the data member with the updated data.
+        self.cards = tempDeck
     }
     
     func cutDeck() -> Void
