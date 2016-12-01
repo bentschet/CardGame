ed02066d9386ad2dc61d3de44568ffe4ddc8a1e5
Benjamin Tschetter <benjamintschetter3@gmail.com>
2016-12-01 13:55:19 -0700

    EMERGENCY COMMIT
    WE GON’ GET WIPED SON

--- CardGame/Model/StupidGame.swift
+++ CardGame/Model/StupidGame.swift
@@ -11,21 +11,55 @@ import Foundation
 class StupidGame
 {
     internal var gameDeck : PlayingCardDeck
-    internal var slotOne : Int
-    internal var slotTwo : Int
+    internal var slotOne : [PlayingCard]
+    internal var slotTwo : [PlayingCard]
     internal var matchChecker : Bool
+    internal var hand : [PlayingCard]
+
     
     init()
     {
         gameDeck = PlayingCardDeck()
-        slotOne = 0
-        slotTwo = 0
+        slotOne = [PlayingCard]()
+        slotTwo = [PlayingCard]()
         matchChecker = false
+        hand = [PlayingCard]()
         
     }
     
     func startGame() -> Void
     {
+        gameDeck.shuffleDeck()
+        drawCards()
+    }
+    
+    private func drawCards() -> Void
+    {
+        hand.append((gameDeck.drawCard() as? PlayingCard)!)
+    }
+    
+    func playGame() -> Void
+    {
         
+    }
+    
+    func checkMatch() -> Bool
+    {
+        var hasMatch : Bool
+        hasMatch = false
+        
+        while gameDeck.cards.count > 0
+        {
+            if(slotOne[0].rank == slotTwo[0].rank)
+            {
+                hasMatch = true
+            }
+            else
+            {
+                hasMatch = false
+            }
+        }
+        
+        return hasMatch
     }
 }
